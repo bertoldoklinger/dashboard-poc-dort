eb245c137ef714197769e9295e809391a6d761eb
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-01-29 11:39:06 -0300

    Add redirects in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/geral',
+        permanent: false,
+      },
+    ]
+  }
 }
 
 export default nextConfig
