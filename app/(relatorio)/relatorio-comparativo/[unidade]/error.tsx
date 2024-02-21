fde5bcd180a6f27bbb68301887443e8d138e2008
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-02-20 22:58:39 -0300

    feat: add error page

---
+++ app/(relatorio)/relatorio-comparativo/[unidade]/error.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Link } from 'lucide-react'
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">Whooops... Ocorreu um erro fatal na aplicação!</h1>
+      <p className="text-accent-foreground">
+        {' '}
+        Voltar para a{' '}
+        <Link href="/home" className="text-sky-600 dark:text-sky-400">
+          Página Inicial
+        </Link>
+      </p>
+    </div>
+  )
+}
